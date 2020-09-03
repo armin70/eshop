3bea3a56d6d42ba4adbfd10b5987e2f9ffbc1dda
armin <armin.a@users.noreply.github.com>
2020-09-03 17:15:10 +0430

    callback

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import order, addToCart, cart, updateCart, deleteOrder, checkout, getInfo, payment, purchase, verify
+from .views import order, addToCart, cart, updateCart, deleteOrder, checkout, getInfo, payment, purchase, verify, callback
 
 app_name = 'order'
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('addtocart/<int:id>/', addToCart, name="add_to_cart"),
     path('cart', cart, name="cart"),
     path('checkout', checkout, name="checkout"),
+    path('callback', callback, name="callback"),
     path('payment', payment, name="payment"),
     path('verify', verify, name="verify"),
     path('purchase', purchase, name="purchase"),
